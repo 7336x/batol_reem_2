e5173838db61a3a98ef20c3981d1386cec03fac8
7336x <R7664alhussaini@gmail.com>
2024-11-04 14:35:32 +0300

    medetation

--- lib/services/tips_services.dart
+++ lib/services/tips_services.dart
@@ -5,7 +5,7 @@ class DioClient {
   static final _baseUrl = 'https://coded-meditation.eapi.joincoded.com';
   static final Dio dio = Dio(BaseOptions(baseUrl: _baseUrl));
 
-  DioClient();
+  DioClient(); //
 
   //DioClient(this.dio);
 
